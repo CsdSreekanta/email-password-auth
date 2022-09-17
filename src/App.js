5fa4dcc34dcbcc2fadb5b288ae5fa8be2b4b078f
HP <sreekanta.csd@gmail.com>
2022-09-17 18:30:01 +0600

    registration form completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import app from "./firebase.init";
 import { getAuth } from "firebase/auth";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const auth = getAuth(app);
 
@@ -19,14 +22,30 @@ function App() {
     console.log("form submitted");
   };
   return (
-    <div className="App">
-      <form onSubmit={handleOnSubmit}>
-        <input onBlur={handleEmailBlur} type="email" placeholder="Enter your email" />
-        <br />
-        <input onBlur={handlePasswordBlur} type="password" placeholder="Enter your password" />
-        <br />
-        <input type="submit" value="log In" />
-      </form>
+    <div>
+       <div className="registration w-50 mx-auto mt-4 border border-primary p-4 rounded">
+        <h1 className="text-primary">Registration</h1>
+       <Form onSubmit={handleOnSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button  variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+       </div>
     </div>
   );
 }
