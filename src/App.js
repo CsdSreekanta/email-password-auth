ef850516471729e85861f39287fab0cdf045e99c
HP <sreekanta.csd@gmail.com>
2022-09-17 18:00:22 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth } from "firebase/auth";
 
-import './App.css';
-import app from './firebase.init';
-import { getAuth} from "firebase/auth";
-
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 function App() {
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+   
+    event.preventDefault()
+    console.log("form submitted");
+  };
   return (
     <div className="App">
-      <form>
-        <input type="text" placeholder='Please Enter You Email' />
-        <input type="password" placeholder='Please Enter Your Password' />
+      <form onSubmit={handleOnSubmit}>
+        <input onBlur={handleEmailBlur} type="email" placeholder="Enter your email" />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" placeholder="Enter your password" />
+        <br />
+        <input type="submit" value="log In" />
       </form>
     </div>
   );
